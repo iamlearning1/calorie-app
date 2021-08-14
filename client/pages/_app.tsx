5a5c50797d5d5df895b5441a7978058d26271ada
iamlearning1 <d1612k@gmail.com>
2021-08-14 13:31:35 +0530

    client setup

---
+++ client/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+
+import { store } from '../app/store';
+
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { Button } from 'antd';
+
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => (
+  <div className={styles.container}>
+    {/* <Button type="default">sdfs</Button> */}
+  </div>
+);
+
+export default Home;
